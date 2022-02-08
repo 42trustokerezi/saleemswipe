ba88f11ee2e90631604b3b223edbbe1a2ce94d89
Trust Okerezi <trustokerezi42@gmail.com>
2022-02-08 20:47:38 +0100

    blog ui created

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,27 +1,30 @@
 import React from 'react';
+import './blog.css'
 
 export default function Blog() {
   return (
     <div className="blog">
         <div className="faq-wrapper">
             <h1>FAQs and Policies</h1>
-            <div className="faq">
-                <a href="">
-                    <h2>Frequently Asked Questions</h2>
-                </a>
-                <p>August 25, 2015</p>
-            </div>
-            <div className="tou">
-                <a href="">
-                    <h2>Terms of Use</h2>
-                </a>
-                <p>August 4, 2015</p>
-            </div>
-            <div className="pri-po">
-                <a href="">
-                    <h2>Privacy Policy</h2>
-                    <p>June 13, 2015</p>
-                </a>
+            <div className="faq-content">
+                <div className="faq">
+                    <a href="">
+                        <h2>Frequently Asked Questions</h2>
+                    </a>
+                    <p>August 25, 2015...</p>
+                </div>
+                <div className="faq">
+                    <a href="">
+                        <h2>Terms of Use...</h2>
+                    </a>
+                    <p>August 4, 2015</p>
+                </div>
+                <div className="faq">
+                    <a href="">
+                        <h2>Privacy Policy</h2>
+                    </a>
+                    <p>June 13, 2015...</p>
+                </div>
             </div>
         </div>
     </div>
---
+++ src/components/Connect/Connect.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './connect.css';
+import facebook fro
+
+export default function Connect() {
+  return (
+    <div>
+        <div>
+            <h2>Connect With Us On:</h2>
+            <div className="socials">
+                <div className="fb">
+                    <a href=""></a>
+                    facebook
+                </div>
+                <div className="tweet">
+                    <a href=""></a>
+                    twitter
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
